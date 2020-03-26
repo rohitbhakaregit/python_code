ef587df4298ea4af20013990d1674c591d4fa555
rohitbhakaregit <bhakarerohit@gmail.com>
2020-03-26 16:37:43 +0530

    removed unwanted file

--- t.sh
+++
@@ -1,7 +0,0 @@
-echo "# python_code" >> README.md
-git init
-git add README.md
-git add .
-git commit -m "first commit"
-git remote add origin https://github.com/rohitbhakaregit/python_code.git
-git push -u origin master
